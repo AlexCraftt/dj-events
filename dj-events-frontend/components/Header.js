f7b5ae49ae7f8aea1156ca0268ab2f91ee229516
Alexandr <toledospb@gmail.com>
2021-08-05 18:34:19 +0300

    events - add event

--- dj-events-frontend/components/Header.js
+++ dj-events-frontend/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Search from '@/components/Search'
 import styles from '@/styles/Header.module.css'
 
 export default function Header() {
@@ -12,12 +13,22 @@ export default function Header() {
           </a>
         </Link>
       </div>
+
+      <Search />
+
       <nav>
         <ul>
           <li>
             <Link href='/events'>
               <a>
                 Events
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/events/add'>
+              <a>
+                Add Event
               </a>
             </Link>
           </li>
